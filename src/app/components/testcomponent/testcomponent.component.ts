9598761fd43d22ddd57fe51da036b35926ba5c44
Evgeniy Konstantinov <Konstantinov-91@mail.ru>
2018-03-02 18:15:50 +1000

    v0.02

--- src/app/components/testcomponent/testcomponent.component.ts
+++ src/app/components/testcomponent/testcomponent.component.ts
@@ -8,8 +8,11 @@ import { Component, OnInit, Input } from '@angular/core';
 export class TestcomponentComponent implements OnInit {
 
   @Input() value: any;
+  viewsize;
 
-  constructor() { }
+  constructor() { 
+    this.viewsize = window.innerWidth;
+  }
 
   ngOnInit() {
   }
